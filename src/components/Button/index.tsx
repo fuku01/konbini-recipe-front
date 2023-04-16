77a5acd721b184ed95d2bcef1c2ea33757af2f91
fuku <>
2023-04-16 23:40:48 +0900

    投稿ページの基本的なバックエンドとの連携処理を追加（タイトルや説明の送信処理）

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,23 +1,9 @@
 import React from 'react'
 
-// type ButtonProps = {
-//   name: string
-// }
 type PostButtonProps = {
   children?: React.ReactNode
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
-
-// export const Button = (props: ButtonProps) => {
-//   return (
-//     <button
-//       className={
-//         'py-2 px-4 bg-orange-500 text-white font-semibold rounded-lg  shadow-md hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-400 focus:ring-opacity-75 w-28 mx-auto'
-//       }
-//     >
-//       {props.name}
-//     </button>
-//   )
-// }
 
 export const PostButton = (props: PostButtonProps) => {
   return (
@@ -25,6 +11,7 @@ export const PostButton = (props: PostButtonProps) => {
       className={
         'py-6 px-5 text-center text-xl rounded-full text-white bg-orange-500 hover:bg-orange-700 shadow-md '
       }
+      onClick={props.onClick}
     >
       {props.children}
     </button>
