09d75b1adbcc0b6d2966a8bd81f21494c5492b34
fuku <77525559+fuku01@users.noreply.github.com>
2023-04-19 18:08:00 +0900

    IDごと個々のレシピページの基本的な機能とレイアウトの実装。 (#5)
    Co-authored-by: fuku <>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className=' bg-orange-50 text-black min-h-screen'>
+    <div className=' bg-orange-50 text-black font-custom min-h-screen'>
       <div className='mx-auto lg:w-1/4 bg-white flex flex-col min-h-screen'>
         <Header />
         <div className='px-8 pt-5 pb-10 flex-1'>
