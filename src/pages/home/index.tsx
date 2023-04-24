a62916e97f5137df7b365729ca21ce93aad112b7
fuku <>
2023-04-24 11:16:01 +0900

    レシピページに編集と削除機能を追加。

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,23 +3,24 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-center'>
-      <div className='font-bold text-2xl mb-20'>仮のページ一覧</div>
-      <Link href='/home'>
-        <div className='font-bold text-xl my-3'>ホーム</div>
-      </Link>
-      <Link href='/myrecipe'>
-        <div className='font-bold text-xl my-3'>マイレシピ</div>
-      </Link>
-      <Link href='/recipe'>
-        <div className='font-bold text-xl my-3'>レシピ詳細</div>
-      </Link>
-      <Link href='/post'>
-        <div className='font-bold text-xl my-3'>投稿</div>
-      </Link>
-      <Link href='/signup'>
-        <div className='font-bold text-xl my-3'>サインアップ</div>
-      </Link>
+    <div>
+      <div className='text-center'>
+        <Link href='/home'>
+          <div className='font-bold text-xl my-3'>ホーム</div>
+        </Link>
+        <Link href='/myrecipe'>
+          <div className='font-bold text-xl my-3'>マイレシピ</div>
+        </Link>
+        <Link href='/post'>
+          <div className='font-bold text-xl my-3'>投稿</div>
+        </Link>
+        <Link href='/signup'>
+          <div className='font-bold text-xl my-3'>サインアップ</div>
+        </Link>
+      </div>
+      <div>人気ランキング</div>
+      <div>新着レシピ</div>
+      <div>閲覧履歴</div>
     </div>
   )
 }
