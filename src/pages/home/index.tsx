401ee3f72572353b240261022881b4399e2774de
fuku <>
2023-05-02 12:26:05 +0900

    add comment

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -18,7 +18,7 @@ type Recipe = {
 const Home = () => {
   const [allRecipe, setAllRecipe] = useState<Recipe[]>([]);
 
-  // レシピの取得
+  // レシピを取得する関数
   const getRecipe = async () => {
     const response = await axios.get<Recipe[]>('/new_recipes');
     setAllRecipe(response.data);
