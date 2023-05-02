1f7f92ea4b6e0a2f6b9209ddc725400d08032041
fuku <>
2023-05-02 16:37:32 +0900

    画面更新時にアイコンサイズが巨大になら無いように修正 （アイコンをdivで囲み、その中でサイズ等を指定）

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,23 +13,25 @@ const Header = () => {
     <div className="sticky top-0 z-40 flex h-14 items-center justify-center bg-[#FCCFA5] text-white shadow-md">
       <div className="relative w-full">
         {router.pathname !== '/home' ? (
-          <FontAwesomeIcon
-            icon={faChevronLeft}
+          <div
             className="absolute left-4 cursor-pointer text-3xl text-black hover:text-orange-500"
             onClick={() => {
               window.history.back();
             }}
-          />
+          >
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </div>
         ) : (
           ''
         )}
-        <FontAwesomeIcon
-          icon={faBars}
+        <div
           className="absolute right-6 cursor-pointer text-3xl text-black hover:text-orange-500"
           onClick={() => {
             setIsMenuOpen(!isMenuOpen);
           }}
-        />
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </div>
         <div className="text-center text-2xl font-bold text-black">
           <Link href="/home">コンビニレシピ</Link>
         </div>
