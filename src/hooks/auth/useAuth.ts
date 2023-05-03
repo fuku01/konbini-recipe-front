90c8b797ca74b64addf2cd438f42c4b03213313e
fuku <>
2023-05-03 19:08:27 +0900

    （保留）ログアウトしてもネットワークのリクエストヘッダーにトークンが付いたままなので修正 #35  ログアウトしたユーザがレシピ編集できてしまうので修正 #36

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -1,23 +1,27 @@
-import { initializeApp } from 'firebase/app'  // Firebaseアプリの初期化を行うためのinitializeApp関数を'firebase/app'からインポート
-import { getAuth, onAuthStateChanged, User } from 'firebase/auth'  // Firebase Authenticationを使用するためのgetAuth関数を'firebase/auth'からインポート
-import { useEffect, useState } from 'react'
+import axios from 'axios';
+import { initializeApp } from 'firebase/app'; // Firebaseアプリの初期化を行うためのinitializeApp関数を'firebase/app'からインポート
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth'; // Firebase Authenticationを使用するためのgetAuth関数を'firebase/auth'からインポート
+import { useEffect, useState } from 'react';
 
-const firebaseConfig = {  // Firebaseアプリの設定情報を変数に代入
+const firebaseConfig = {
+  // Firebaseアプリの設定情報を変数に代入
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-}
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
 
-const useAuth = () => {  // useAuthという名前の関数を定義する
-  const app = initializeApp(firebaseConfig)  // Firebaseアプリの初期化
-  const auth = getAuth(app)  // Firebase Authenticationの認証オブジェクトを取得
+const useAuth = () => {
+  // useAuthという名前の関数を定義する
+  const app = initializeApp(firebaseConfig); // Firebaseアプリの初期化
+  const auth = getAuth(app); // Firebase Authenticationの認証オブジェクトを取得
   const [currentUser, setCurrentUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true); // ローディング状態を追加
 
+  // ログイン状態の変化を監視
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -27,7 +31,16 @@ const useAuth = () => {  // useAuthという名前の関数を定義する
     });
   }, [auth]);
 
-  return {auth, currentUser, loading}  // 認証オブジェクトを返す
-}
+  useEffect(() => {
+    // ログインユーザーがいる場合
+    if (currentUser) {
+      currentUser.getIdToken().then((token) => {
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      });
+    }
+  }, [currentUser]);
 
-export default useAuth  // useAuth関数をデフォルトエクスポートする
+  return { auth, currentUser, loading, }; // 認証オブジェクトを返す
+};
+
+export default useAuth; // useAuth関数をデフォルトエクスポートする
