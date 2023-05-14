1e5524138cdbdf68b41a1e7594a275ac470866e6
fuku <77525559+fuku01@users.noreply.github.com>
2023-05-14 23:18:15 +0900

    マイレシピ一覧の表示を修正。 (#129)
    いいね数が表示されるように追加。
    
    Co-authored-by: fuku <>

--- src/components/TextFormArea/index.tsx
+++ src/components/TextFormArea/index.tsx
@@ -18,13 +18,13 @@ const TextFormArea = (props: TextFormProps) => {
         <textarea
           placeholder={placeholder}
           value={props.value} // ここにvalueプロパティを追加（編集ページの初期値表示のために必要）
-          className="border-blue-gray-200 text-blue-gray-700 placeholder-shown:border-blue-gray-200 disabled:bg-blue-gray-50 peer mt-5 h-full min-h-[200px] w-full resize-none rounded-none bg-transparent pb-1.5 pt-6 text-base font-normal outline outline-0 transition-all focus:border-orange-500 focus:outline-0 disabled:resize-none"
+          className="border-blue-gray-200 text-blue-gray-700 placeholder-shown:border-blue-gray-200 disabled:bg-blue-gray-50 peer mt-5 h-full min-h-[200px] w-full resize-none rounded-none border-b bg-transparent pb-1.5 pt-6 text-base font-normal outline outline-0 transition-all focus:border-orange-500 focus:outline-0 disabled:resize-none"
           onChange={props.onChange}
           maxLength={props.maxLength}
         ></textarea>
 
         {label && (
-          <label className="after:content[' '] peer-disabled:peer-placeholder-shown:text-blue-gray-500 pointer-events-none absolute -top-2.5 left-0 flex h-full w-full select-none text-base font-normal leading-tight transition-all after:absolute after:-bottom-1 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-orange-500 after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-focus:text-sm peer-focus:leading-tight peer-focus:text-orange-500 peer-focus:after:scale-x-100 peer-focus:after:border-orange-500 peer-disabled:text-transparent">
+          <label className="after:content[' '] peer-disabled:peer-placeholder-shown:text-blue-gray-500 pointer-events-none absolute -top-2.5 left-0 flex h-full w-full select-none text-base font-normal leading-tight transition-all after:absolute after:-bottom-1 after:block after:w-full after:scale-x-0 after:border-b after:border-orange-500 after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-focus:text-sm peer-focus:leading-tight peer-focus:text-orange-500 peer-focus:after:scale-x-100 peer-focus:after:border-orange-500 peer-disabled:text-transparent">
             {label}
           </label>
         )}
