6254422bb55c58b1cd37ae08e9c9d9555d5bdf2d
fuku <>
2023-05-15 12:07:43 +0900

    ユーザーがいいね登録したレシピを表示する「お気に入りリスト」ページを新規作成。#131

---
+++ src/pages/favoriterecipe/index.tsx
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import React, { useCallback, useEffect, useState } from 'react';
+import RcipeList from '@/components/RecipeList';
+
+type Recipe = {
+  id: number;
+  user_id: number;
+  title: string;
+  content: string;
+  time: number;
+  calorie: number;
+  image: string;
+  created_at: Date;
+  updated_at: Date;
+  price: number;
+  favorites_count: number;
+};
+
+const FavoriteRecipe = () => {
+  const [favorite, setFavorite] = useState<Recipe[]>([]);
+
+  // マイレシピを取得する関数
+  const getFavoriteRecipes = useCallback(async () => {
+    try {
+      const response = await axios.get<Recipe[]>('favorite_recipes');
+      setFavorite(response.data);
+      console.log('お気に入りレシピの取得に成功しました', response.data);
+    } catch (error) {
+      console.log('お気に入りレシピの取得に失敗しました', error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getFavoriteRecipes();
+  }, [getFavoriteRecipes]);
+
+  return <RcipeList recipes={favorite} />;
+};
+export default FavoriteRecipe;
