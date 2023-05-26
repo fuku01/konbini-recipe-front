01ac5090a91ca60b44554debf4774b12c0d8b77e
fuku <>
2023-05-26 17:05:43 +0900

    使い方ページを新規作成する。#182

--- src/pages/guidance/index.tsx
+++ src/pages/guidance/index.tsx
@@ -1,6 +1,11 @@
-import { faFileLines, faHeart, faPen } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCircleQuestion,
+  faFileLines,
+  faHeart,
+  faPen,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import Link from 'next/link';
+import Link from 'next/link';
 import React from 'react';
 import LoginButton from '@/components/Sidemenu/LoginButton';
 import SignupButton from '@/components/Sidemenu/SignupButton';
@@ -48,8 +53,24 @@ const Guidance = () => {
             </div>
           </div>
         </div>
+        <div className="mt-6 text-center">
+          <p className="ml-5 text-sm font-bold text-orange-500">
+            詳しい使い方はこちらをクリック
+          </p>
+          <Link href="/information" className="hover:group">
+            <div className="group mt-1 text-lg underline transition duration-75 ease-in-out hover:text-orange-500">
+              <span className="rounded-lg px-1 py-0.5 hover:bg-[#FDF1DE] ">
+                <FontAwesomeIcon
+                  icon={faCircleQuestion}
+                  className="mr-1 w-5 group-hover:scale-110"
+                />
+                使い方
+              </span>
+            </div>
+          </Link>
+        </div>
         {/* -----------------------------ログインの案内--------------------------------- */}
-        <div className="mx-6 mb-8 mt-6 rounded-2xl bg-[#FDF1DE] pb-10 pt-5 text-center shadow-md">
+        <div className="mx-6 mb-8 mt-4 rounded-2xl bg-[#FDF1DE] pb-10 pt-5 text-center shadow-md">
           <div className="mx-2 flex justify-around">
             <div>
               <span className="text-xs">初めての方はこちら</span>
@@ -60,15 +81,6 @@ const Guidance = () => {
               <LoginButton />
             </div>
           </div>
-          {/* <Link href="/home" className="hover:group">
-          <div className="group ml-3 mr-14 mt-6 rounded-xl py-1.5 pl-4 transition duration-75 ease-in-out hover:bg-[#FBB87F] hover:text-orange-500">
-            <FontAwesomeIcon
-              icon={faCircleQuestion}
-              className="mr-2 w-5 group-hover:scale-110"
-            />
-            使い方
-          </div>
-        </Link> */}
           <div className="mt-10 scale-125">
             <p className="mb-1 text-xs">まずは試してみる！</p>
             <GuestLoginButton />
